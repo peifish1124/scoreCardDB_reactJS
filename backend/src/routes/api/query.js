c746f08f397565113e441360dc20e5a863e366ed
peifish1124 <b08705026@ntu.edu.tw>
2022-02-06 12:58:59 +0800

    Add files via upload

---
+++ backend/src/routes/api/query.js
@@ -0,0 +1,29 @@
+import ScoreCard from "../../models/ScoreCard.js";
+
+const query = async (type, queryString) => {
+    var nameExisting, subExisting;
+    var output = [];
+    if(type === 'name'){
+        nameExisting = await ScoreCard.find({ name: queryString });
+        if(nameExisting.length === 0){
+            return false;
+        } else {
+            for(var i = 0; i < nameExisting.length; i++){
+                output[i] = `Found card with name: (${nameExisting[i].name},${nameExisting[i].subject},${nameExisting[i].score})`;
+            }
+            return output;
+        } 
+    } else {
+        subExisting = await ScoreCard.find({ subject: queryString });
+        if(subExisting.length === 0){
+            return false;
+        } else {
+            for(var i = 0; i < subExisting.length; i++){
+                output[i] = `Found card with subject: (${subExisting[i].name},${subExisting[i].subject},${subExisting[i].score})`;
+            }
+            return output;
+        }
+    }
+};
+
+export default query;
---
+++ backend/src/routes/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import add from './api/add.js';
+import clear from './api/clear.js';
+import query from './api/query.js';
+
+const router = express.Router();
+
+router.post('/create-card', async (req, res) => {
+    const name = req.body.name;
+    const subject = req.body.subject;
+    const score = parseInt(req.body.score);
+
+    const card = await add(name, subject, score);
+    if(card === 'updating'){
+        res.json({ message: `Updating (${name}, ${subject}, ${score})`,card: card });
+    } else{
+        res.json({ message: `Adding (${name}, ${subject}, ${score})`,card: card });
+    } 
+})
+router.delete('/clear-db', async (req, res) => {
+    const result = await clear();
+    res.json({ message: result });
+})
+router.get('/query-cards', async (req, res) => {
+    const type = req.query.type;
+    const queryString = req.query.queryString;
+    const upperType = type.charAt(0).toUpperCase() + type.slice(1);
+
+    const result = await query(type, queryString);
+    if(!result)
+        res.json({ message: `${upperType} (${queryString}) not found!` });
+    else{
+        res.json({ messages: result }); 
+    }
+})
+export default router;
